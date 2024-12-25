2656e6f0da598e60856e2cd4be087cbf6a051fb4
Sumit Pareek <sumit-macbook@Sumits-MacBook-Air.local>
2024-12-25 12:55:17 +0530

    scheduler with fixed delay

---
+++ shedlock-main/src/main/java/org/sumitbytes/shedlock_main/GreetingsJob.java
@@ -0,0 +1,18 @@
+package org.sumitbytes.shedlock_main;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Component
+public class GreetingsJob {
+
+    @Scheduled(fixedDelay = 5000)
+    public void greetingsJob(){
+
+        log.info("Hello there !!! Greetings for the day its {}, have a good day ahead :)", LocalDateTime.now());
+    }
+}
---
+++ shedlock-main/src/test/java/org/sumitbytes/shedlock_main/ShedlockMainApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sumitbytes.shedlock_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShedlockMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
